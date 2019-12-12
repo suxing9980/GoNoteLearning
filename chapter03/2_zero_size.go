4ef079e69aafa14cbca6db849f6389af5f2e9e1a
qinglin9980 <suxing9980@163.com>
2019-12-12 17:15:59 +0800

    add pointer and zero_size

---
+++ chapter03/2_zero_size.go
@@ -0,0 +1,13 @@
+package main
+
+// shift + f10 :执行
+import "fmt"
+
+// @Time    : 2019/12/12 17:11
+// @Author  : Litte柯南
+// @File    : 2_zero_size.go
+func main() {
+	var a, b struct{}
+	// 对象的地址是否相等与具体版本有关
+	fmt.Println(&a == &b, &a == nil)
+}
