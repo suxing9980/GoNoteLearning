9fb984bf6828e7e7ef1c5428a775eaf317a69ee4
qinglin9980 <suxing9980@163.com>
2019-12-13 10:21:16 +0800

    ifelse and forrange

---
+++ chapter03/6_for_range.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// @Time    : 2019/12/13 10:18
+// @Author  : Litte柯南
+// @File    : 6_for_range.go
+
+func main() {
+	var data = [3]string{"a", "b", "c"}
+	for index, item := range data {
+		fmt.Println(index, item)
+	}
+}
